5c07b7c9ea749f142d1727601661fa6cce3b8aa5
rohitturkar <rohittur2002@gmail.com>
2024-11-28 19:56:47 +0530

    setup database connection and express connection

---
+++ src/db/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const db = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+
+    console.log(`\n MongoDB connected !! DB Host : ${db.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB Connection Error", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
